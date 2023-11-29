ac5b0de11cd6a9c13acd910e574a1339b4ab5009
Andrew Srg <sergienkoandrew9922@gmail.com>
2023-11-29 17:26:42 +0200

    feat: add message queue health check script

---
+++ src/mqhandler/infrastructure/mq/health_check.py
@@ -0,0 +1,38 @@
+import asyncio
+import sys
+import time
+
+import aio_pika
+from aiormq import AMQPConnectionError
+
+from mqhandler.adapters.settings import SettingsRepo
+
+
+async def check_connection():
+    settings = SettingsRepo()
+
+    address = await settings.get("AMQP_ADDRESS", raise_exc=True)
+    port = await settings.get("AMQP_PORT", raise_exc=True)
+    user = await settings.get("AMQP_USER", raise_exc=True)
+    password = await settings.get("AMQP_PASSWORD", raise_exc=True)
+
+    url = f"ampq://{user}:{password}@{address}:{port}/"
+
+    print(url)
+
+    rollback = 1
+    while rollback <= 16:
+        try:
+            await aio_pika.connect_robust(url)
+            print("Successfully connection")
+            sys.exit(0)
+        except AMQPConnectionError:
+            print(f"Connection failed. Try again after {rollback} sec.")
+            time.sleep(rollback)
+            rollback *= 2
+    print("Connection filed. Exit.")
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    asyncio.run(check_connection())
